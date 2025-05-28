56087a5362c059d4cfa6051e73525d34eb8f5a4a
JadoonTeam-Dev <jadoongg6@gmail.com>
2025-05-28 10:02:22 +0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -351,7 +351,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363354023106228@newsletter',
+                        newsletterJid: '120363358664877093@newsletter',
                         newsletterName: config.BOT_NAME,
                         serverMessageId: 143
                     }
